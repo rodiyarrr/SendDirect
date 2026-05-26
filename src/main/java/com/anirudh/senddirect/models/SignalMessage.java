5500dd95f84e251b980ec26e3640e2e08b5c6ec6
Anirudh Singh Shekhawat <rodiyarrr@gmail.com>
2026-05-26 17:07:54 +0530

    Implemented sendOffer() and sendReceiver function()

--- src/main/java/com/anirudh/senddirect/models/SignalMessage.java
+++ src/main/java/com/anirudh/senddirect/models/SignalMessage.java
@@ -5,5 +5,8 @@ import lombok.Data;
 @Data
 public class SignalMessage {
     private String sender;
+    private String shareCode;
     private String content;
+    private String type;
+
 }
--- src/main/java/com/anirudh/senddirect/websocket/SignalController.java
+++ src/main/java/com/anirudh/senddirect/websocket/SignalController.java
@@ -1,15 +1,36 @@
 package com.anirudh.senddirect.websocket;
 
 import com.anirudh.senddirect.models.SignalMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class SignalController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
     @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public SignalMessage sendMessage(SignalMessage message){
-        return message;
+    public void sendMessage(
+            SignalMessage message
+    ){
+
+        System.out.println(
+                "Received: "
+                        + message.getContent()
+        );
+
+        messagingTemplate.convertAndSend(
+                "/topic/session/"
+                        + message.getShareCode(),
+
+                message
+        );
+
     }
+
 }
