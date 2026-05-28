270cc85dcebe6550189128ebe18b15437cb649c9
Anirudh Singh Shekhawat <rodiyarrr@gmail.com>
2026-05-28 16:54:59 +0530

    Bug fixes

--- src/main/java/com/anirudh/senddirect/exceptions/SessionNotFoundException.java
+++ src/main/java/com/anirudh/senddirect/exceptions/SessionNotFoundException.java
@@ -1,7 +1,10 @@
 package com.anirudh.senddirect.exceptions;
 
-public class SessionNotFoundException extends RuntimeException{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class SessionNotFoundException extends RuntimeException{
     public SessionNotFoundException(String message) {
         super(message);
     }
